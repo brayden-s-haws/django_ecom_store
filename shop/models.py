4e2d9fc9667d590ff1078883447008376e7104b5
brayden-s-haws <haws@hey.com>
2025-12-16 19:23:56 -0700

    Initialize 'shop' app with basic files and register in settings.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=200)
+    price = models.FloatField()
+    discounted_price = models.FloatField()
+    image = models.CharField(max_length=300)
