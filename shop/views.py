cc55b40996eb86fc9efe7534353f1ba115835cdd
brayden-s-haws <haws@hey.com>
2025-12-17 19:43:09 -0700

    Add Product model, admin registration, and basic shop page layout with styles and Bootstrap integration.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+
+from .models import Product
 
 # Create your views here.
+def index(request):
+    product_objects = Product.objects.all()
+    return render(request, 'shop/index.html', {'product_objects': product_objects})
